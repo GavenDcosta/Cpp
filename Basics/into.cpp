8b56edbbc139e3ba95dda8ac2bf4e0630259da11
Gaven Dcosta <gavendcosta@gmail.com>
2023-11-10 10:45:50 +0530

    first commit

---
+++ Basics/into.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main(){   //main is the entry point of a program
+  int a;
+  std::cout<<"Hello World";
+  std::cout<<"Enter a number";
+  std::cin>>a;
+  std::cout<<a;
+  return 0;
+}
---
+++ Basics/tut1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//this is a comment
+/* this
+is
+a
+multipliesline
+comment */
+
+int main(){
+    int sum = 6;
+    cout<<"Hello World"<< sum;
+    return 0;
+}
