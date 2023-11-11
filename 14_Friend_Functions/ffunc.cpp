4c1cd59753fdf10c42446eca509258951fb80fb7
Gaven Dcosta <gavendcosta@gmail.com>
2023-11-11 12:12:20 +0530

    friends

--- 14_Friend_Functions/ffunc.cpp
+++ 14_Friend_Functions/ffunc.cpp
@@ -11,6 +11,7 @@ class Complex{
         b=n2;
       }
 
+      // Below line means that non member - sumComplex function is allowed to do anything with my private data  
       friend Complex sumComplex(Complex o1, Complex o2);
 
       void printNumber(){
@@ -34,3 +35,16 @@ int main(){
     sum.printNumber();
     return 0;
 }
+
+
+//Properties of Friend Functions
+/*
+
+1. Not in the scope of Class
+2. Since it is not in the scope of the class it cannot be called from the object of that class => c1.sunComplex --> INVALID
+3. Can be invoked without the help of any object
+4. Usuaklly contains the objects as arguments
+5. Can be declared inside the public or the private section of the class
+6. It cannot access the members directly by their names and need object_name.member_name
+
+*/
